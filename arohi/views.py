fc392d58ec73b7ce205b7484254bee83bfab8dcb
zunaedsifat <zunaed.sifat@gmail.com>
2018-12-22 16:32:07 +0600

    added login, consumer & investor feed in arohi/views

--- arohi/views.py
+++ arohi/views.py
@@ -1,3 +1,26 @@
+from .models import Product, Entrepreneur
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'templates/auth/login.html')
+
+
+def consumer_feed(request):
+    try:
+        product_list = Product.objects.all()
+    except Product.DoesNotExist:
+        raise Http404("Products does not exist.")
+
+    return render(request, 'templates/feed/consumer.html', {'product_list': product_list})
+
+
+def investor_feed(request):
+    try:
+        entrepreneur_list = Entrepreneur.objects.all()
+    except Entrepreneur.DoesNotExist:
+        raise Http404("Entrepreneurs don't exist in the database.")
+
+    return render(request, 'templates/feed/entrepreneur.html', {'entrepreneur_list':entrepreneur_list})
+
