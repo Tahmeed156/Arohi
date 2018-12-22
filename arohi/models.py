85cefdefe0a33daece7c55e6a6b2b990d35f41f9
Tahmeed Tarek <tahmeedtarek@gmail.com>
2018-12-22 17:25:03 +0600

    Added cards for products

--- arohi/models.py
+++ arohi/models.py
@@ -29,7 +29,26 @@ class Product(models.Model):
     category = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     price = models.IntegerField()
-    producer = models.ForeignKey(Investor, on_delete=models.CASCADE)
+    # producer = models.ForeignKey(, on_delete=models.CASCADE)
     rating_sum = models.IntegerField()
     rating_cnt = models.IntegerField()
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+
+        for i in range(count):
+            product = Product(
+                name=forgery_py.name.industry(),
+                category=forgery_py.lorem_ipsum.words(randint(3, 5)),
+                description=forgery_py.lorem_ipsum.words(randint(5, 15)),
+                price=(randint(1, 100)),
+                # producer_id=1,
+                rating_sum=(randint(50, 100)),
+                rating_cnt=(randint(15, 30)),
+            )
+            product.save()
 
